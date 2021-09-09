3fe768eca8b8b8cf9e62c783257e432b4d833020
CNFeffery <fefferypzy@gmail.com>
2021-09-09 18:03:25 +0800

    新增AntdInput文档

--- views/AntdInput.py
+++ views/AntdInput.py
@@ -515,6 +515,40 @@ docs_content = html.Div(
                                         html.Span(id='input-nClicksSearch-demo-output')
                                     ]
                                 )
+                                ...
+                                @app.callback(
+                                    Output('input-value-demo-output', 'children'),
+                                    Input('input-value-demo', 'value'),
+                                    prevent_initial_call=True
+                                )
+                                def input_value_callback_demo(value):
+                                
+                                    return fac.AntdText(f'value: {value}', italic=True)
+                                
+                                
+                                @app.callback(
+                                    Output('input-nSubmit-demo-output', 'children'),
+                                    Input('input-nSubmit-demo', 'nSubmit'),
+                                    State('input-nSubmit-demo', 'value'),
+                                    prevent_initial_call=True
+                                )
+                                def input_nSubmit_callback_demo(nSubmit, value):
+                                
+                                    if nSubmit and value:
+                                        return fac.AntdText(f'nSubmit: {nSubmit}   value: {value}', italic=True)
+                                
+                                
+                                @app.callback(
+                                    Output('input-nClicksSearch-demo-output', 'children'),
+                                    Input('input-nClicksSearch-demo', 'nClicksSearch'),
+                                    State('input-nClicksSearch-demo', 'value'),
+                                    prevent_initial_call=True
+                                )
+                                def input_nClicksSearch_callback_demo(nClicksSearch, value):
+
+                                    if nClicksSearch and value:
+                                        return fac.AntdText(f'nClicksSearch: {nClicksSearch}   value: {value}', italic=True)
+
                                 ```
                                 '''),
                     title='点击查看代码',
