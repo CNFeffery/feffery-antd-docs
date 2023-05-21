95453133bcf6fb6d8399da3110ac6238f3597065
CNFeffery <fefferypzy@gmail.com>
2023-05-21 22:12:56 +0800

    perf: 新增cdn静态资源主（unpkg）备（饿了么）机制

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import os
+import re
 import dash
 from flask import request, abort
 
@@ -8,27 +9,43 @@ from config import Config
 class CustomDash(dash.Dash):
 
     def interpolate_index(self, **kwargs):
-        # scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'https://unpkg.zhimg.com/')
-        scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'https://npm.elemecdn.com/')
-        # scripts = kwargs.pop('scripts').replace('https://unpkg.com/', 'https://fastly.jsdelivr.net/npm/')
-
-        # scripts = kwargs.pop('scripts')
-
-        # # 提取scripts部分符合条件的外部js资源
-        # external_scripts = re.findall('(<script src=".*?"></script>)', scripts)
-
-        # # 将原有的script标签内容替换为带备用地址错误切换的版本
-        # for external_script in external_scripts:
-        #     scripts = scripts.replace(
-        #         external_script,
-        #         '''<script src="{}" onerror="console.log(1111); this.onerror = null; this.src = '{}'"></script>'''.format(
-        #             # 默认地址
-        #             re.findall('"(.*?)"', external_script)[0]
-        #             .replace('https://unpkg.com/', 'http://npm.elemecd.com/'),
-        #             # 备用地址
-        #             re.findall('"(.*?)"', external_script)[0]
-        #         )
+        # scripts = (
+        #     kwargs.pop('scripts').replace(
+        #         'https://unpkg.com/', 'https://npm.elemecdn.com/'
         #     )
+        # )
+        scripts = kwargs.pop('scripts')
+
+        # 提取scripts部分符合条件的外部js资源
+        external_scripts = re.findall(
+            '(<script src="http.*?"></script>)', scripts)
+
+        # 将原有的script标签内容替换为带备用地址错误切换的版本
+        for external_script in external_scripts:
+            scripts = scripts.replace(
+                external_script,
+                '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("footer").appendChild(fallbackScript);'></script>'''.format(
+                    re.findall('"(.*?)"', external_script)[0],
+                    re.findall('"(.*?)"', external_script)[0]
+                    .replace('https://unpkg.com/', 'https://npm.elemecdn.com/')
+                )
+            )
+
+        scripts = '''<script>
+window.onerror = async function(message, source, lineno, colno, error) { 
+    if ( message.includes("DashRenderer") !== -1 ) {
+        while ( true ) {
+            try {
+                var renderer = new DashRenderer();
+                break
+            } catch {
+                setTimeout("", 250)
+            }
+        }
+    }
+}
+</script>
+''' + scripts
 
         return super(CustomDash, self).interpolate_index(scripts=scripts, **kwargs)
 
