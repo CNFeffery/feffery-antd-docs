8ff756642b89b73f1b10f35160c8b8dc438c171a
CNFeffery <fefferypzy@gmail.com>
2023-05-23 16:59:25 +0800

    perf: 优化备用cdn切换策略

--- server.py
+++ server.py
@@ -18,30 +18,53 @@ class CustomDash(dash.Dash):
 
         # 提取scripts部分符合条件的外部js资源
         external_scripts = re.findall(
-            '(<script src="http.*?"></script>)', scripts)
+            '(<script src="http.*?"></script>)',
+            scripts
+        )
 
         # 将原有的script标签内容替换为带备用地址错误切换的版本
         for external_script in external_scripts:
             scripts = scripts.replace(
                 external_script,
-                '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("footer").appendChild(fallbackScript);'></script>'''.format(
-                    re.findall('"(.*?)"', external_script)[0],
+                '''<script src="{}" onerror='this.remove(); let fallbackScript = document.createElement("script"); fallbackScript.src = "{}"; document.querySelector("head").prepend(fallbackScript);'></script>'''.format(
+                    re.findall('"(.*?)"', external_script)[0]
+                    .replace('https://unpkg.com/', 'https://unpk.com/'),
                     re.findall('"(.*?)"', external_script)[0]
                     .replace('https://unpkg.com/', 'https://npm.elemecdn.com/')
                 )
             )
 
         scripts = '''<script>
-window.onerror = async function(message, source, lineno, colno, error) { 
-    if ( message.includes("DashRenderer") !== -1 ) {
-        while ( typeof renderer === 'undefined' ) {
-            try {
-                var renderer = new DashRenderer();
-            } catch {
-                await new Promise(resolve => setTimeout(resolve, 200));
-            }
-        }
+window.onerror = async function(message, source, lineno, colno, error) {
+    if (message.includes('is not defined') !== -1) {
+        await waitForModules();
     }
+}
+
+async function waitForModules() {
+    const requiredModules = [
+        'DashRenderer',
+        'dash_html_components',
+        'dash_core_components',
+        'feffery_antd_components',
+        'feffery_utils_components',
+        'feffery_markdown_components'
+    ];
+
+    while (!areModulesDefined(requiredModules)) {
+        await delay(100); // 延迟100毫秒
+    }
+
+    // 变量已定义，触发事件
+    var renderer = new DashRenderer();
+}
+
+function areModulesDefined(modules) {
+    return modules.every(module => window[module]);
+}
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 </script>
 ''' + scripts
