9a36cfc0cbfc8a3b92b89112f850b126d69cbb45
CNFeffery <fefferypzy@gmail.com>
2024-08-12 15:10:40 +0800

    refactor: 重构各组件文档页渲染逻辑，统一通过components.demos_render()进行渲染

---
+++ components/demos_render.py
@@ -0,0 +1,194 @@
+from dash import html, dcc
+import feffery_antd_components as fac
+import feffery_utils_components as fuc
+from dash.dependencies import Component
+import feffery_markdown_components as fmc
+
+import views
+from config import AppConfig
+from components import mock_browser
+
+
+def render(
+    component: Component, demos_config: list, section_name: str = None
+) -> Component:
+    """渲染组件文档页示例内容"""
+
+    return fac.AntdSpace(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        (
+                            fac.AntdSpace(
+                                [
+                                    mock_browser.render(),
+                                    # 懒加载优化
+                                    fuc.FefferyLazyLoad(
+                                        html.Iframe(
+                                            src='/~demo/{}/{}'.format(
+                                                component.__name__, item['path']
+                                            ),
+                                            style={
+                                                'border': '1px solid #f0f0f0',
+                                                'boxSizing': 'border-box',
+                                                'width': '100%',
+                                                'height': 350,
+                                            },
+                                        ),
+                                        height=350,
+                                        throttle=500,
+                                    ),
+                                ],
+                                direction='vertical',
+                                size=0,
+                                style={'width': '100%'},
+                            )
+                            if item.get('iframe')
+                            else getattr(
+                                getattr(
+                                    views, section_name or component.__name__
+                                ).demos,
+                                item['path'],
+                            ).render()
+                        ),
+                        className='demo-box',
+                    ),
+                    html.Div(
+                        [
+                            fac.AntdText(
+                                [
+                                    item['title'],
+                                    fac.AntdTooltip(
+                                        html.A(
+                                            fac.AntdIcon(
+                                                icon='antd-edit',
+                                                className='demo-github-link',
+                                            ),
+                                            href='{}/edit/{}/views/{}/demos/{}.py'.format(
+                                                AppConfig.doc_library_repo,
+                                                AppConfig.doc_library_branch,
+                                                section_name
+                                                or component.__name__,
+                                                item['path'],
+                                            ),
+                                            target='_blank',
+                                        ),
+                                        title='在Github上编辑此示例',
+                                    ),
+                                ],
+                                className='demo-title',
+                            ),
+                            html.Div(
+                                item['description'],
+                                style={'padding': '18px 24px 12px 28px'},
+                            ),
+                        ],
+                        style={'position': 'relative'},
+                    ),
+                    fac.AntdCenter(
+                        fac.AntdSpace(
+                            [
+                                fac.AntdTooltip(
+                                    dcc.Link(
+                                        fac.AntdIcon(icon='antd-export'),
+                                        href='/~demo/{}/{}'.format(
+                                            section_name or component.__name__,
+                                            item['path'],
+                                        ),
+                                        target='_blank',
+                                        className='demo-operations-icon',
+                                    ),
+                                    title='在新窗口打开',
+                                ),
+                                fac.AntdTooltip(
+                                    fac.AntdIcon(
+                                        id={
+                                            'type': 'demo-code-toggle',
+                                            'index': '{}/{}'.format(
+                                                component.__name__,
+                                                item['path'],
+                                            ),
+                                        },
+                                        icon='antd-code',
+                                        className='demo-operations-icon',
+                                    ),
+                                    title='展开/收起代码',
+                                ),
+                                fac.AntdTooltip(
+                                    dcc.Link(
+                                        fac.AntdIcon(icon='antd-github'),
+                                        href='{}/tree/{}/views/{}/demos/{}.py'.format(
+                                            AppConfig.doc_library_repo,
+                                            AppConfig.doc_library_branch,
+                                            section_name or component.__name__,
+                                            item['path'],
+                                        ),
+                                        target='_blank',
+                                        className='demo-operations-icon',
+                                    ),
+                                    title='在Github中查看完整代码',
+                                ),
+                                fac.AntdTooltip(
+                                    dcc.Link(
+                                        fac.AntdIcon(
+                                            icon='si-gitee',
+                                            style={'verticalAlign': '-0.3em'},
+                                        ),
+                                        href='{}/blob/{}/views/{}/demos/{}.py'.format(
+                                            AppConfig.doc_gitee_library_repo,
+                                            AppConfig.doc_library_branch,
+                                            section_name or component.__name__,
+                                            item['path'],
+                                        ),
+                                        target='_blank',
+                                        className='demo-operations-icon',
+                                    ),
+                                    title='在Gitee中查看完整代码',
+                                ),
+                            ],
+                            size=12,
+                        ),
+                        className='demo-operations',
+                    ),
+                    fac.AntdTabs(
+                        items=[
+                            {
+                                'label': item.get('language') or 'Python',
+                                'key': item.get('language') or 'Python',
+                                'children': fmc.FefferySyntaxHighlighter(
+                                    codeString=item['code'],
+                                    language=(
+                                        item.get('language') or 'Python'
+                                    ).lower(),
+                                    codeTheme='coy-without-shadows',
+                                    codeBlockStyle={'overflowX': 'auto'},
+                                ),
+                            }
+                            for item in getattr(
+                                getattr(
+                                    views, section_name or component.__name__
+                                ).demos,
+                                item['path'],
+                            ).code_string
+                        ],
+                        centered=True,
+                        className='demo-code-tabs',
+                        id={
+                            'type': 'demo-code',
+                            'index': '{}/{}'.format(
+                                component.__name__, item['path']
+                            ),
+                        },
+                        style={'display': 'none'},
+                    ),
+                ],
+                className='demo-container',
+                id='demo-container-' + item['path'],
+            )
+            for item in demos_config
+        ],
+        direction='vertical',
+        size='large',
+        style={'width': '100%'},
+    )
--- views/AntdImage/demos/__init__.py
+++ views/AntdImage/demos/__init__.py
@@ -1,16 +1,13 @@
-from dash import html, dcc
 import feffery_antd_components as fac
 from dash.dependencies import Component
-import feffery_markdown_components as fmc
 
-import views
 from . import (
     basic_usage,  # noqa: F401
     preview_false,  # noqa: F401
     multi,  # noqa: F401
     fallback,  # noqa: F401
 )
-from config import AppConfig
+from components import demos_render
 
 demos_config = [
     {
@@ -58,146 +55,11 @@ demos_config = [
 ]
 
 
-def render(component: Component) -> Component:
+def render(component: Component, section_name: str = None) -> Component:
     """渲染当前组件演示用例"""
 
-    return fac.AntdSpace(
-        [
-            html.Div(
-                [
-                    html.Div(
-                        getattr(views.AntdImage.demos, item['path']).render(),
-                        className='demo-box',
-                    ),
-                    html.Div(
-                        [
-                            fac.AntdText(
-                                [
-                                    item['title'],
-                                    fac.AntdTooltip(
-                                        html.A(
-                                            fac.AntdIcon(
-                                                icon='antd-edit',
-                                                className='demo-github-link',
-                                            ),
-                                            href='{}/edit/{}/views/{}/demos/{}.py'.format(
-                                                AppConfig.doc_library_repo,
-                                                AppConfig.doc_library_branch,
-                                                component.__name__,
-                                                item['path'],
-                                            ),
-                                            target='_blank',
-                                        ),
-                                        title='在Github上编辑此示例',
-                                    ),
-                                ],
-                                className='demo-title',
-                            ),
-                            html.Div(
-                                item['description'],
-                                style={'padding': '18px 24px 12px 28px'},
-                            ),
-                        ],
-                        style={'position': 'relative'},
-                    ),
-                    fac.AntdCenter(
-                        fac.AntdSpace(
-                            [
-                                fac.AntdTooltip(
-                                    dcc.Link(
-                                        fac.AntdIcon(icon='antd-export'),
-                                        href='/~demo/{}/{}'.format(
-                                            component.__name__, item['path']
-                                        ),
-                                        target='_blank',
-                                        className='demo-operations-icon',
-                                    ),
-                                    title='在新窗口打开',
-                                ),
-                                fac.AntdTooltip(
-                                    fac.AntdIcon(
-                                        id={
-                                            'type': 'demo-code-toggle',
-                                            'index': '{}/{}'.format(
-                                                component.__name__, item['path']
-                                            ),
-                                        },
-                                        icon='antd-code',
-                                        className='demo-operations-icon',
-                                    ),
-                                    title='展开/收起代码',
-                                ),
-                                fac.AntdTooltip(
-                                    dcc.Link(
-                                        fac.AntdIcon(icon='antd-github'),
-                                        href='{}/tree/{}/views/{}/demos/{}.py'.format(
-                                            AppConfig.doc_library_repo,
-                                            AppConfig.doc_library_branch,
-                                            component.__name__,
-                                            item['path'],
-                                        ),
-                                        target='_blank',
-                                        className='demo-operations-icon',
-                                    ),
-                                    title='在Github中查看完整代码',
-                                ),
-                                fac.AntdTooltip(
-                                    dcc.Link(
-                                        fac.AntdIcon(
-                                            icon='si-gitee',
-                                            style={'verticalAlign': '-0.3em'},
-                                        ),
-                                        href='{}/blob/{}/views/{}/demos/{}.py'.format(
-                                            AppConfig.doc_gitee_library_repo,
-                                            AppConfig.doc_library_branch,
-                                            component.__name__,
-                                            item['path'],
-                                        ),
-                                        target='_blank',
-                                        className='demo-operations-icon',
-                                    ),
-                                    title='在Gitee中查看完整代码',
-                                ),
-                            ],
-                            size=12,
-                        ),
-                        className='demo-operations',
-                    ),
-                    fac.AntdTabs(
-                        items=[
-                            {
-                                'label': item.get('language') or 'Python',
-                                'key': item.get('language') or 'Python',
-                                'children': fmc.FefferySyntaxHighlighter(
-                                    codeString=item['code'],
-                                    language=(
-                                        item.get('language') or 'Python'
-                                    ).lower(),
-                                    codeTheme='coy-without-shadows',
-                                    codeBlockStyle={'overflowX': 'auto'},
-                                ),
-                            }
-                            for item in getattr(
-                                views.AntdImage.demos, item['path']
-                            ).code_string
-                        ],
-                        centered=True,
-                        className='demo-code-tabs',
-                        id={
-                            'type': 'demo-code',
-                            'index': '{}/{}'.format(
-                                component.__name__, item['path']
-                            ),
-                        },
-                        style={'display': 'none'},
-                    ),
-                ],
-                className='demo-container',
-                id='demo-container-' + item['path'],
-            )
-            for item in demos_config
-        ],
-        direction='vertical',
-        size='large',
-        style={'width': '100%'},
+    return demos_render.render(
+        component=component,
+        demos_config=demos_config,
+        section_name=section_name,
     )
