4473fe9fa0faff6e6bff09d887dcd9c8251fb69d
CNFeffery <fefferypzy@gmail.com>
2024-08-19 22:01:01 +0800

    doc: 更新getting_started示例代码

--- views/getting_started.py
+++ views/getting_started.py
@@ -146,79 +146,60 @@ app.layout = html.Div(
             fac.AntdForm(
                 [
                     fac.AntdFormItem(
-                        fac.AntdInputNumber(
-                            id='target-value',
-                            style={
-                                'width': '100%'
-                            }
-                        ),
-                        label='目标值'
+                        fac.AntdInputNumber(id="target-value", style={"width": "100%"}),
+                        label="目标值",
                     ),
                     fac.AntdFormItem(
-                        fac.AntdInputNumber(
-                            id='actual-value',
-                            style={
-                                'width': '100%'
-                            }
-                        ),
-                        label='实际值'
-                    )
+                        fac.AntdInputNumber(id="actual-value", style={"width": "100%"}),
+                        label="实际值",
+                    ),
                 ],
-                layout='inline',
-                style={
-                    'marginBottom': 25,
-                    'width': '100%'
-                }
+                layout="inline",
+                style={"marginBottom": 25, "width": "100%"},
             ),
             # 输出目标容器
             html.Div(
-                id='output-container',
+                id="output-container",
                 style={
                     # 基于css中的flex布局实现水平垂直居中
-                    'width': '100%',
-                    'display': 'flex',
-                    'justifyContent': 'center',
-                    'alignItems': 'center'
-                }
-            )
+                    "width": "100%",
+                    "display": "flex",
+                    "justifyContent": "center",
+                    "alignItems": "center",
+                },
+            ),
         ],
-        title='dash+fac应用示例',
+        title="dash+fac应用示例",
         hoverable=True,
         style={
             # 这里利用到css中的fixed布局
-            'position': 'fixed',
-            'top': '40%',
-            'left': '50%',
-            'transform': 'translate(-50%, -50%)',
-            'width': 540,
-            'height': 350
-        }
+            "position": "fixed",
+            "top": "40%",
+            "left": "50%",
+            "transform": "translate(-50%, -50%)",
+            "width": 560,
+            "height": 350,
+        },
     )
 )
+
 
 # 定义回调函数串起相关交互逻辑
 @app.callback(
-    Output('output-container', 'children'),
-    [Input('target-value', 'value'),
-     Input('actual-value', 'value')]
+    Output("output-container", "children"),
+    [Input("target-value", "value"), Input("actual-value", "value")],
 )
 def handle_progress_render(target_value, actual_value):
-
     # 判断传入的目标值和实际值是否有效
     if target_value and actual_value:
-
         return fac.AntdProgress(
-            percent=round(100 * actual_value / target_value, 2),
-            type='dashboard'
+            percent=round(100 * actual_value / target_value, 2), type="dashboard"
         )
 
-    return fac.AntdResult(
-        subTitle='请输入有效的目标值和实际值',
-        status='warning'
-    )
+    return fac.AntdResult(subTitle="请输入有效的目标值和实际值", status="warning")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False)
 """,
                                     language='python',
