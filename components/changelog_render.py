d82f3d63cc25bd902ff74cf56295016118cb1fec
CNFeffery <fefferypzy@gmail.com>
2024-08-23 09:45:12 +0800

    doc: changelog i18n

--- components/changelog_render.py
+++ components/changelog_render.py
@@ -1,12 +1,24 @@
 from dash import html
 import feffery_markdown_components as fmc
 
+from i18n import get_current_locale
+
 
 def render(version: str):
     """渲染更新日志页面"""
 
-    with open('./changelogs/{}.md'.format(version), 'r', encoding='utf-8') as f:
-        changelog = f.read()
+    current_locale = get_current_locale()
+
+    if current_locale == 'zh-cn':
+        with open(
+            './changelogs/{}.md'.format(version), 'r', encoding='utf-8'
+        ) as f:
+            changelog = f.read()
+    elif current_locale == 'en-us':
+        with open(
+            './changelogs/en_us/{}.md'.format(version), 'r', encoding='utf-8'
+        ) as f:
+            changelog = f.read()
 
     return html.Div(
         fmc.FefferyMarkdown(markdownStr=changelog),
