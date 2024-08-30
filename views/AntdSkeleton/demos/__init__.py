4cb42ad4daee23522fd0827da2f559272d66deb6
CNFeffery <fefferypzy@gmail.com>
2024-08-30 14:50:11 +0800

    chore: 调整部分组件示例description为字符型

--- views/AntdSkeleton/demos/__init__.py
+++ views/AntdSkeleton/demos/__init__.py
@@ -1,4 +1,3 @@
-import feffery_antd_components as fac
 from dash.dependencies import Component
 
 from . import (
@@ -14,53 +13,27 @@ demos_config = [
     {
         'path': 'basic_usage',
         'title': '基础使用',
-        'description': fac.AntdParagraph(
-            '默认情况下，骨架屏会监听其内部的各个组件是否处于回调函数运算输出中状态，从而自动控制加载状态的切换。'
-        ),
+        'description': '默认情况下，骨架屏会监听其内部的各个组件是否处于回调函数运算输出中状态，从而自动控制加载状态的切换。',
     },
     {
         'path': 'active',
         'title': '动态扫屏效果',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('active=True', code=True),
-                '为骨架屏添加动态扫屏效果。',
-            ]
-        ),
+        'description': '设置参数`active=True`为骨架屏添加动态扫屏效果。',
     },
     {
         'path': 'custom_widgets',
         'title': '调整骨架屏不同部分',
-        'description': fac.AntdParagraph(
-            '对骨架屏中表达不同内容的部分进行灵活调整。'
-        ),
+        'description': '对骨架屏中表达不同内容的部分进行灵活调整。',
     },
     {
         'path': 'exclude_mode',
         'title': '排除监听模式',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('listenPropsMode="exclude"', code=True),
-                '开启排除监听模式，该模式下骨架屏将忽略监听参数',
-                fac.AntdText('excludeProps', code=True),
-                '所定义的组件属性更新过程。',
-            ]
-        ),
+        'description': "设置参数`listenPropsMode='exclude'`开启排除监听模式，该模式下骨架屏将忽略监听参数`excludeProps`所定义的组件属性更新过程。",
     },
     {
         'path': 'include_mode',
         'title': '包含监听模式',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('listenPropsMode="include"', code=True),
-                '开启包含监听模式，该模式下骨架屏将只监听参数',
-                fac.AntdText('includeProps', code=True),
-                '所定义的组件属性更新过程。',
-            ]
-        ),
+        'description': "设置参数`listenPropsMode='include'`开启包含监听模式，该模式下骨架屏将只监听参数`includeProps`所定义的组件属性更新过程。",
     },
 ]
 
--- views/AntdSlider/demos/__init__.py
+++ views/AntdSlider/demos/__init__.py
@@ -1,4 +1,3 @@
-import feffery_antd_components as fac
 from dash.dependencies import Component
 
 from . import (
@@ -19,103 +18,52 @@ demos_config = [
     {
         'path': 'basic_usage',
         'title': '基础使用',
-        'description': fac.AntdParagraph(
-            [
-                '基本滑动条。当',
-                fac.AntdText('range=True', code=True),
-                '时，渲染为双滑块。',
-            ]
-        ),
+        'description': '基本滑动条。当`range=True`时，渲染为双滑块。',
     },
     {
         'path': 'vertical_mode',
         'title': '垂直模式',
-        'description': fac.AntdParagraph('垂直方向的滑动条。'),
+        'description': '垂直方向的滑动条。',
     },
     {
         'path': 'custom_step',
         'title': '滑动步长',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('step', code=True),
-                '自定义滑动步长。',
-            ]
-        ),
+        'description': '设置参数`step`自定义滑动步长。',
     },
     {
         'path': 'custom_marks',
         'title': '刻度标签',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('marks', code=True),
-                '自定义刻度标签。',
-            ]
-        ),
+        'description': '设置参数`marks`自定义刻度标签。',
     },
     {
         'path': 'tooltip_visible',
         'title': '滑动提示内容显示策略',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('tooltipVisible', code=True),
-                '显示或者隐藏滑动提示内容。',
-            ]
-        ),
+        'description': '设置参数`tooltipVisible`显示或者隐藏滑动提示内容。',
     },
     {
         'path': 'tooltip_prefix_suffix',
         'title': '滑动提示内容前后缀',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('tooltipPrefix', code=True),
-                '控制滑动提示内容前缀。',
-                '设置参数',
-                fac.AntdText('tooltipSuffix', code=True),
-                '控制滑动提示内容后缀。',
-            ]
-        ),
+        'description': '设置参数`tooltipPrefix`控制滑动提示内容前缀。设置参数`tooltipSuffix`控制滑动提示内容后缀。',
     },
     {
         'path': 'disabled_status',
         'title': '禁用状态',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('disabled=True', code=True),
-                '开启禁用状态。',
-            ]
-        ),
+        'description': '设置参数`disabled=True`开启禁用状态。',
     },
     {
         'path': 'read_only_status',
         'title': '只读状态',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('readOnly=True', code=True),
-                '开启只读状态。',
-            ]
-        ),
+        'description': '设置参数`readOnly=True`开启只读状态。',
     },
     {
         'path': 'rail_style',
         'title': '滑轨样式',
-        'description': fac.AntdParagraph(
-            [
-                '设置参数',
-                fac.AntdText('railStyle', code=True),
-                '自定义滑轨样式。',
-            ]
-        ),
+        'description': '设置参数`railStyle`自定义滑轨样式。',
     },
     {
         'path': 'basic_callbacks',
         'title': '回调监听',
-        'description': fac.AntdParagraph('可用于监听滑动条滑动事件。'),
+        'description': '监听滑动条滑动事件。',
     },
 ]
 
