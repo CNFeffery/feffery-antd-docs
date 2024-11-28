989c11bec462e675a521913121ea8de94fb497e7
CNFeffery <fefferypzy@gmail.com>
2024-11-28 17:02:25 +0800

    doc: AntdDatePicker文档也新增示例“内嵌后缀图标”

---
+++ views/AntdDatePicker/demos/suffix_icon.py
@@ -0,0 +1,24 @@
+import feffery_antd_components as fac
+from dash.dependencies import Component
+
+
+def render() -> Component:
+    """渲染当前演示用例"""
+
+    # 构造演示用例相关内容
+    demo_contents = fac.AntdDatePicker(
+        value='2024-01-01', suffixIcon=fac.AntdIcon(icon='antd-user')
+    )
+
+    return demo_contents
+
+
+code_string = [
+    {
+        'code': """
+fac.AntdDatePicker(
+    value='2024-01-01', suffixIcon=fac.AntdIcon(icon='antd-user')
+)
+"""
+    }
+]
