f817a4ff67a016a349b17c23fc15aa6f6ea12c17
CNFeffery <fefferypzy@gmail.com>
2024-11-28 17:07:56 +0800

    doc: AntdDateRangePicker文档页新增示例“内嵌前缀内容”

--- views/AntdDateRangePicker/demos/__init__.py
+++ views/AntdDateRangePicker/demos/__init__.py
@@ -8,6 +8,7 @@ from . import (
     custom_format,  # noqa: F401
     disabled,  # noqa: F401
     read_only,  # noqa: F401
+    prefix,  # noqa: F401
     render_status,  # noqa: F401
     picker_value,  # noqa: F401
     disabled_dates,  # noqa: F401
@@ -53,6 +54,11 @@ demos_config = [
         'path': 'read_only',
         'title': '只读状态',
         'description': '设置参数`readOnly=True`开启只读状态。',
+    },
+    {
+        'path': 'prefix',
+        'title': '内嵌前缀内容',
+        'description': '通过参数`prefix`设置选择框内嵌前缀内容。',
     },
     {
         'path': 'render_status',
