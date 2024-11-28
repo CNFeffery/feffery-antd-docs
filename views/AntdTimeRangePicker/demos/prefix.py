e259438126a2941f7b6d82be6ff40eb28c40dbe3
CNFeffery <fefferypzy@gmail.com>
2024-11-28 17:30:33 +0800

    doc: AntdTimeRangePicker文档页新增示例“内嵌前缀内容”

---
+++ views/AntdTimeRangePicker/demos/prefix.py
@@ -0,0 +1,26 @@
+import feffery_antd_components as fac
+from dash.dependencies import Component
+
+
+def render() -> Component:
+    """渲染当前演示用例"""
+
+    # 构造演示用例相关内容
+    demo_contents = fac.AntdTimeRangePicker(
+        defaultValue=['11:00:00', '12:00:00'],
+        prefix=fac.AntdIcon(icon='antd-user'),
+    )
+
+    return demo_contents
+
+
+code_string = [
+    {
+        'code': """
+fac.AntdTimeRangePicker(
+    defaultValue=['11:00:00', '12:00:00'],
+    prefix=fac.AntdIcon(icon='antd-user'),
+)
+"""
+    }
+]
