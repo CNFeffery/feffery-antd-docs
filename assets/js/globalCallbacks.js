2c36f0b997f64f1035411cf406180860ea49a212
CNFeffery <fefferypzy@gmail.com>
2025-07-17 17:46:55 +0800

    chore: 基于dash_console_filter_plugin替代原有的浏览器控制台错误屏蔽策略

--- assets/js/globalCallbacks.js
+++ assets/js/globalCallbacks.js
@@ -1,14 +1,3 @@
-// 改造console.error()以隐藏无关痛痒的警告信息
-const originalConsoleError = console.error;
-console.error = function (...args) {
-    // 检查args中是否包含需要过滤的内容
-    const shouldFilter = args.some(arg => typeof arg === 'string' && arg.includes('Warning:'));
-
-    if (!shouldFilter) {
-        originalConsoleError.apply(console, args);
-    }
-};
-
 window.dash_clientside = Object.assign({}, window.dash_clientside, {
     clientside: {
         toggleSideMenuVisible: (nClicks, originIcon) => {
