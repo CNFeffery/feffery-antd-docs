94d2060f073b409dc4ecdbf417476c8203719a94
CNFeffery <fefferypzy@gmail.com>
2025-08-21 11:15:16 +0800

    chore: 移除侧边菜单栏额外附着说明内容

--- components/side_menu.py
+++ components/side_menu.py
@@ -3,9 +3,6 @@ import feffery_antd_components as fac
 
 from config import AppConfig
 
-# 国际化
-from i18n import translator
-
 
 def render(locale: str = 'zh-cn'):
     """渲染侧边导航菜单"""
@@ -18,78 +15,6 @@ def render(locale: str = 'zh-cn'):
                         fac.AntdMenu(
                             id='side-menu',
                             menuItems=AppConfig.side_menu_items(),
-                            menuItemKeyToTitle={
-                                **{
-                                    '/' + value.split(' ')[0]: fac.AntdRow(
-                                        [
-                                            fac.AntdCol(value),
-                                            fac.AntdCol(
-                                                fac.AntdTooltip(
-                                                    fac.AntdTag(
-                                                        content=fac.AntdIcon(
-                                                            icon='antd-app-store-add'
-                                                        ),
-                                                        color='green',
-                                                    ),
-                                                    title=translator.t(
-                                                        '0.3.x版本新增组件'
-                                                    ),
-                                                    placement='right',
-                                                )
-                                            ),
-                                        ],
-                                        justify='space-between',
-                                        wrap=False,
-                                    )
-                                    for value in [
-                                        translator.t(
-                                            'AntdFloatButton 悬浮按钮'
-                                        ),
-                                        translator.t(
-                                            'AntdFloatButtonGroup 悬浮按钮组'
-                                        ),
-                                        translator.t('AntdFlex 弹性布局'),
-                                        translator.t('AntdSplitter 分隔面板'),
-                                        translator.t(
-                                            'AntdColorPicker 颜色选择'
-                                        ),
-                                        translator.t(
-                                            'AntdCheckableTag 可选择标签'
-                                        ),
-                                        translator.t('AntdImageGroup 图片组合'),
-                                        translator.t('AntdQRCode 二维码'),
-                                        translator.t('AntdTour 漫游式引导'),
-                                        translator.t('Fragment 空节点'),
-                                    ]
-                                },
-                                **{
-                                    '/' + value.split(' ')[0]: fac.AntdRow(
-                                        [
-                                            fac.AntdCol(value),
-                                            fac.AntdCol(
-                                                fac.AntdTooltip(
-                                                    fac.AntdTag(
-                                                        content=fac.AntdIcon(
-                                                            icon='antd-experiment'
-                                                        ),
-                                                        color='blue',
-                                                    ),
-                                                    title=translator.t(
-                                                        '实验性质组件'
-                                                    ),
-                                                    placement='right',
-                                                )
-                                            ),
-                                        ],
-                                        justify='space-between',
-                                        wrap=False,
-                                    )
-                                    for value in [
-                                        'AntdDraggablePanel 可拖拽面板',
-                                        'AntdEditorLayout 编辑器布局',
-                                    ]
-                                },
-                            },
                             mode='inline',
                             inlineIndent=12,
                             style={'borderRight': 'none'},
