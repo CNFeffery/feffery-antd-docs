decec36702e786912b9bc6d90b4b9b4b3395d083
CNFeffery <fefferypzy@gmail.com>
2025-08-21 16:36:25 +0800

    doc: AntdTable再渲染文档页新增示例“控制进度条尺寸”

--- views/AntdTableRerender/demos/__init__.py
+++ views/AntdTableRerender/demos/__init__.py
@@ -21,6 +21,7 @@ from . import (
     mini_progress_percent_position,  # noqa: F401
     mini_progress_percent_precision,  # noqa: F401
     mini_progress_round,  # noqa: F401
+    mini_progress_size,  # noqa: F401
     mini_ring_progress_mode,  # noqa: F401
     button_mode_and_callbacks,  # noqa: F401
     button_mode_color,  # noqa: F401
@@ -138,6 +139,12 @@ demos_config = [
         'path': 'mini_progress_round',
         'title': '圆角矩形风格',
         'description': '基于配置项`progressStrokeLinecap`控制进度条风格。',
+        'group': 'mini-progress迷你进度图模式',
+    },
+    {
+        'path': 'mini_progress_size',
+        'title': '控制进度条尺寸',
+        'description': '基于配置项`progressSize`控制进度条像素尺寸。',
         'group': 'mini-progress迷你进度图模式',
     },
     {
---
+++ views/AntdTableRerender/demos/mini_progress_size.py
@@ -0,0 +1,74 @@
+import feffery_antd_components as fac
+from dash.dependencies import Component
+
+
+def render() -> Component:
+    """渲染当前演示用例"""
+
+    # 构造演示用例相关内容
+    demo_contents = fac.AntdTable(
+        columns=[
+            {
+                'title': 'mini-progress示例1',
+                'dataIndex': 'mini-progress示例1',
+                'renderOptions': {
+                    'renderType': 'mini-progress',
+                    'progressSize': 24,
+                },
+            },
+            {
+                'title': 'mini-progress示例2',
+                'dataIndex': 'mini-progress示例2',
+                'renderOptions': {
+                    'renderType': 'mini-progress',
+                    'progressShowPercent': True,
+                    'progressStrokeLinecap': 'round',
+                    'progressSize': 24,
+                },
+            },
+        ],
+        data=[
+            {'mini-progress示例1': x, 'mini-progress示例2': x}
+            for x in [0, 0.66, 1]
+        ],
+        bordered=True,
+        tableLayout='fixed',
+    )
+
+    return demo_contents
+
+
+code_string = [
+    {
+        'code': """
+fac.AntdTable(
+    columns=[
+        {
+            'title': 'mini-progress示例1',
+            'dataIndex': 'mini-progress示例1',
+            'renderOptions': {
+                'renderType': 'mini-progress',
+                'progressSize': 24,
+            },
+        },
+        {
+            'title': 'mini-progress示例2',
+            'dataIndex': 'mini-progress示例2',
+            'renderOptions': {
+                'renderType': 'mini-progress',
+                'progressShowPercent': True,
+                'progressStrokeLinecap': 'round',
+                'progressSize': 24,
+            },
+        },
+    ],
+    data=[
+        {'mini-progress示例1': x, 'mini-progress示例2': x}
+        for x in [0, 0.66, 1]
+    ],
+    bordered=True,
+    tableLayout='fixed',
+)
+"""
+    }
+]
