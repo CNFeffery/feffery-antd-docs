dd114a422d1e473042b3a7dfec525842d6e19ca9
CNFeffery <fefferypzy@gmail.com>
2025-08-21 11:18:40 +0800

    doc: 修正AntdProgress顶部面包屑分类错误

--- views/AntdProgress/intro.py
+++ views/AntdProgress/intro.py
@@ -8,8 +8,7 @@ def render() -> Component:
         fac.AntdBreadcrumb(
             items=[
                 {'title': '组件介绍'},
-                {'title': '通用'},
-                {'title': '排版相关'},
+                {'title': '反馈'},
                 {'title': 'AntdProgress 进度条'},
             ],
             style={'marginBottom': 8},
