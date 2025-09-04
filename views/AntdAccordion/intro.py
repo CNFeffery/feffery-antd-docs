602935116c2b9a695619fada184c6254b91208b5
GhaziXX <gtounsi52@gmail.com>
2025-09-04 13:22:24 +0200

    translate demo

--- views/AntdAccordion/intro.py
+++ views/AntdAccordion/intro.py
@@ -1,18 +1,20 @@
 import feffery_antd_components as fac
 from dash.dependencies import Component
 
+from i18n import translator
+
 
 def render() -> Component:
     """渲染组件介绍内容"""
     return [
         fac.AntdBreadcrumb(
             items=[
-                {'title': '组件介绍'},
-                {'title': '数据展示'},
-                {'title': 'AntdAccordion 手风琴'},
+                {"title": translator.t("组件介绍")},
+                {"title": translator.t("数据展示")},
+                {"title": translator.t("AntdAccordion 手风琴")},
             ],
-            style={'marginBottom': 8},
+            style={"marginBottom": 8},
         ),
-        fac.AntdTitle('AntdAccordion 手风琴', level=2),
-        fac.AntdParagraph('展示一组同级内容的特殊形式。'),
+        fac.AntdTitle(translator.t("AntdAccordion 手风琴"), level=2),
+        fac.AntdParagraph(translator.t("展示一组同级内容的特殊形式。")),
     ]
